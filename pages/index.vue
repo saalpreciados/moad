4445ad40324507716b7ba3c14cd9c243c2dc5a9d
Carles Ibáñez Golfe <carles2612@gmail.com>
2020-02-23 19:34:56 +0100

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <div>
+      <logo />
+      <h1 class="title">
+        moad
+      </h1>
+      <h2 class="subtitle">
+        museu online d&#39;art digital
+      </h2>
+      <div class="links">
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">
+          GitHub</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
