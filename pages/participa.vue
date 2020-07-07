76e3254aba3a4e7a375f44c465d60b3f2caf6a4f
Carles Ibáñez Golfe <carles2612@gmail.com>
2020-07-07 17:57:56 +0200

    Add participa page

---
+++ pages/participa.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container participa">
+    <h1>Participa en el museu</h1>
+    <p>
+      Tot i haver nascut com a projecte individual, el Museu Online d'Art Digital vol  créixer i aconvertir-se ser una institució participativa, col·lectiva i oberta.
+    </p>
+    <p>
+      Ara mateix estem posant en marxa aquesta pàgina on s'allotjarà tot la informació sobre com col·laborar amb nosaltres, sigues artista, estudiant, investigador, desenvolupador, o a aficionat a l'art. Hi ha lloc per a tots.
+    </p>
+    <p>
+      De moment, et pots posar en contacte amb nosaltres a <a href="mailto:moad@imcarl.es">moad@imcarl.es</a>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'default-footer'
+}
+</script>
+
+<style lang="scss" scoped>
+  .participa {
+
+    p {
+      font-size: 1.25rem;
+      line-height: 1.7;
+    }
+
+    h1 {
+      text-align: left;
+    }
+  }
+</style>
